6cf449f33b1ffa8ac04527c980b31afd1381550b
yanchao86 <yanchao@idotools.com>
2016-01-18 18:15:15 +0800

    test

--- ToolboxService/src/main/java/com/pixshow/toolboxmgr/action/AppConfigAction.java
+++ ToolboxService/src/main/java/com/pixshow/toolboxmgr/action/AppConfigAction.java
@@ -64,6 +64,9 @@ public class AppConfigAction extends BaseAction {
         String query = request.getQueryString();
 
         for (int i = 0; i < keys.length; i++) {
+            
+            
+            //#################
             String code = keys[i];
             code = code.substring(0, code.indexOf("_"));
             if (list.get(i) == null) {
